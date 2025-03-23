f9cd1ed6480c1d22c6695648ced031591201b1ac
maciejs13 <maciej.sledzinski@cyfroweszkoly.pl>
2025-03-23 19:18:39 +0100

    Szkielet programu i klasa tworzaca zadanie

--- Daily Planner/MainWindow.axaml.cs
+++ Daily Planner/MainWindow.axaml.cs
@@ -2,6 +2,14 @@ using Avalonia.Controls;
 
 namespace Daily_Planner;
 
+public class Zadanie
+{
+    private bool _czyUkonczone = false;
+    
+    public string Nazwa { get; set; }
+    public string Kategorie { get; set; }
+}
+ 
 public partial class MainWindow : Window
 {
     public MainWindow()
